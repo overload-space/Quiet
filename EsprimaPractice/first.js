0ed976ccd1f1ba7a9d8edfbbdc145273b671632f
Yuehan Liu <lyhlgf93@gmail.com>
2016-12-05 15:57:15 +0800

    First commit

--- EsprimaPractice/first.js
+++ EsprimaPractice/first.js
@@ -1,2 +1,19 @@
 var esprima = require('esprima');
-console.log(JSON.stringify(esprima.tokenize('var answer = 42'), null, 4));
+var fs = require('fs');
+
+fs.readFile('test.js', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    var parseTree = esprima.parse(data.toString(), { loc : true });
+    for (var child in parseTree) {
+
+    }
+
+    fs.writeFile('parseTree.json', JSON.stringify(parseTree), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
