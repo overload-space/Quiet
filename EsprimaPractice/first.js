7d746a73ca04f2a341d5e78160f56054373f2160
Yuehan Liu <lyhlgf93@gmail.com>
2016-12-28 01:49:19 +0800

    First commit

--- EsprimaPractice/first.js
+++ EsprimaPractice/first.js
@@ -1,4 +1,6 @@
+var escope = require('escope');
 var esprima = require('esprima');
+var estraverse = require('estraverse');
 var fs = require('fs');
 
 fs.readFile('test.js', function (err, data) {
@@ -7,13 +9,50 @@ fs.readFile('test.js', function (err, data) {
     }
 
     var parseTree = esprima.parse(data.toString(), { loc : true });
-    for (var child in parseTree) {
+    var scopeManager = escope.analyze(parseTree);
+    var currentScope = scopeManager.acquire(parseTree);   // global scope
+    var notYet = false;
+    estraverse.traverse(parseTree, {
+        enter: function (node, parent) {
 
-    }
-
-    fs.writeFile('parseTree.json', JSON.stringify(parseTree), function (err) {
-        if (err) {
-            return console.log(err);
+        },
+        leave: function (node, parent) {
+            if (/Identifier/.test(node.type) && node.name === 'color') {
+                console.log("\n\n\n");
+                console.log(node);
+                console.log("\n\n\n");
+            }
+            currentScope = scopeManager.acquire(node);
+            if (currentScope !== null) {
+                for (var x in currentScope.variables) {
+                    if (currentScope.variables[x].name === 'color') {
+                        console.log(currentScope);
+                    }
+                }
+            }
         }
     });
+
+    // estraverse.traverse(parseTree, {
+    //     enter: function(node, parent) {
+    //         // do stuff
+    //
+    //         if (/Function/.test(node.type)) {
+    //             currentScope = scopeManager.acquire(node);  // get current function scope
+    //         }
+    //     },
+    //     leave: function(node, parent) {
+    //         if (/Function/.test(node.type)) {
+    //             currentScope = currentScope.upper;  // set to parent scope
+    //         }
+    //
+    //         // do stuff
+    //     }
+    //});
+
+    // fs.writeFile('parseTree.json', JSON.stringify(parseTree), function (err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    // });
 });
--- EsprimaPractice/test.js
+++ EsprimaPractice/test.js
@@ -1,149 +1,9 @@
-/**
- * Created by jcx on 16/3/4.
- */
-
 "undefined" == typeof(element_block_js) && !function() {
     window.element_block_js = "element_block_js";
     function elementBlock() {
-        var self = this;
-        /**
-         *  idle: 0
-         *  select: 1    wait for user to move mouse and click
-         *  adjust: 2    user sue panel to get biger and small area
-         */
-        var EBSTATE_IDLE = 0, EBSTATE_SELECT = 1, EBSTATE_ADJUST = 2;
-        var mode = EBSTATE_IDLE;
-        var onObj;
         var color = {};
         color.i = 1;
         color.c = "#000080";
-
-        var lastmouse = {};
-
-
-        document.addEventListener('mousemove', function (e) {
-            if (mode !== EBSTATE_SELECT) {
-                return;
-            }
-
-            if (lastmouse.x == e.clientX && lastmouse.y == e.clientY) {
-                return;
-            }
-
-            clearBlock();
-
-            var x = e.clientX, y = e.clientY;
-            var elementMouseIsOver = document.elementFromPoint(x, y);
-
-            showEBlock(elementMouseIsOver);
-            lastmouse.x = e.clientX;
-            lastmouse.y = e.clientY;
-            onObj = elementMouseIsOver;
-
-            return false;
-        }, true);
-
-        function clearBlock() {
-            jQuery('jQueryClone' + color.i).remove();
-        }
-
-        function showEBlock(obj) {
-            if (obj == document) {
-                console.log("hover is document");
-                return;
-            }
-            if (obj == undefined) {
-                console.log("element is undefined");
-                return;
-            }
-
-            if (!jQuery('#jqueryClones').length) {
-                jQuery("<div id='jqueryClones'></div>").appendTo('body');
-            }
-
-            console.log(obj);
-
-            var offset = jQuery(obj).offset();
-            jQuery('<jQueryClone' + color.i + '></jQueryClone' + color.i + '>')
-                .css({
-                    width: jQuery(obj).outerWidth(),
-                    height: jQuery(obj).outerHeight(),
-                    top: offset.top - 3,
-                    left: offset.left - 3,
-                    position: 'absolute',
-                    zIndex: 99999999999999999,
-                    opacity: 0.3,
-                    background: color.c,
-                    border: '0px dashed white'
-                }).appendTo('#jqueryClones');
-
-        }
-
-        document.addEventListener('click', function (e) {
-            if (mode == EBSTATE_SELECT) {
-                mode = EBSTATE_ADJUST;
-
-                /*  TODO: show a new adjust panel    */
-                self.adjustState(onObj);
-
-                e.stopPropagation();
-                e.preventDefault();
-                return;
-            }
-        }, true);
-
-        document.addEventListener("keyup", function (e) {
-            if (e.keyCode == 27 && mode == EBSTATE_SELECT) {
-                mode = EBSTATE_IDLE;
-                clearBlock();
-                ShowBNPanel();
-                return
-            }
-        });
-
-        this.clearEBlock = function () {
-            jQuery('jQueryClone' + color.i).remove();
-        };
-
-        this.activate = function () {
-            mode = EBSTATE_SELECT;
-            HideBNPanel();
-        };
-
-        this.adjustState = function () {
-            clearBlock();
-            ShowBNAdjustPanel(onObj);
-        };
-
-        this.adjustMessageHandler = function (msg) {
-            if (msg.cmd == "zoomdown") {
-                RemoveBNAdjustPanel();
-                onObj = childNode(onObj);
-                ShowBNAdjustPanel(onObj);
-            } else if (msg.cmd == "zoomup") {
-                RemoveBNAdjustPanel();
-                onObj = parentNode(onObj);
-                ShowBNAdjustPanel(onObj);
-            } else if (msg.cmd === "presibling") {
-                RemoveBNAdjustPanel();
-                onObj = getSibling(onObj, 0);
-                ShowBNAdjustPanel(onObj);
-            } else if (msg.cmd === "nextsibling") {
-                RemoveBNAdjustPanel();
-                onObj = getSibling(onObj, 1);
-                ShowBNAdjustPanel(onObj);
-            } else if (msg.cmd == "finish") {
-                RemoveBNAdjustPanel();
-                var sobj = GenerateObject.generateObject(onObj);
-                sobj.name = msg.name;
-                getSelectionData(sobj);
-                ShowBNPanel();
-            } else if (msg.cmd == "cancel") {
-                RemoveBNAdjustPanel();
-                ShowBNPanel();
-            }
-
-        };
 
         function parentNode(obj) {
             if (obj.tagName.toLowerCase() == "body" && obj.tagName.toLowerCase() == "html") {
@@ -191,7 +51,6 @@
             return objnext;
         }
 
-        // mode ==  0, get pre; mode == 1, get next
         function getSibling(obj, mode) {
             var originobj = obj;
             var objnext = iterSibling(obj, mode);
@@ -201,15 +60,7 @@
                 if (objnext == originobj)
                     break;
             }
-
             return objnext;
         }
-
-        //Object.preventExtensions(this);
-        return;
     }
-
-
-    window.eBlock = new elementBlock();
-
 }();
